5210a244d7c375c4da22939c84321c82780d278a
Nikhil Keshri <95970358+Nikhil-keshri2213@users.noreply.github.com>
2025-10-10 14:43:00 +0000

    category update

--- PracticeUi/src/pages/Category/CategoryPanel.jsx
+++ PracticeUi/src/pages/Category/CategoryPanel.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import data from '../../data/data.json'
+import CategoryList from './CategoryList';
 
 const CategoryPanel = () => {
+
+ const [categories, setCategories] = useState(data?.categories);
+
   return (
-    <div>CategoryPanel</div>
+    <div className="p-6 bg-gray-100 min-h-screen rounded-lg">
+      <div className="flex justify-between items-center mb-6">
+          <h2 className="text-2xl font-bold text-gray-800">Category</h2>
+          <button className="px-4 py-1 text-green-500 font-semibold border rounded-md hover:bg-green-500 hover:text-white transition">
+            Create +
+          </button>
+        </div>
+
+        <div>
+          <CategoryList categoryTypeList={categories?.categoryTypeList} name={categories?.name}/>
+        </div>
+    </div>
   )
 }
 
